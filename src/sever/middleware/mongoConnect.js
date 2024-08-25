7a0812e23b0d1387dffec4937664af7aef932ea0
Alan <93069058+alanjuice@users.noreply.github.com>
2024-08-25 18:03:38 +0530

    remove mongodb key

--- src/sever/middleware/mongoConnect.js
+++ src/sever/middleware/mongoConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURL = 'mongodb://alanjose2025:alan2003@ac-hyowbse-shard-00-00.2y09nal.mongodb.net:27017,ac-hyowbse-shard-00-01.2y09nal.mongodb.net:27017,ac-hyowbse-shard-00-02.2y09nal.mongodb.net:27017/?ssl=true&replicaSet=atlas-13u53v-shard-0&authSource=admin&retryWrites=true&w=majority';
+const mongoURL = 'your mongo url here';
 async function mongoConnect() {
     //connect to mongodb
     const response = await mongoose.connect(mongoURL);
